233ca942c5a6513205f5464a6423afaef4aa77f3
luv <luvy2814@gmail.com>
2020-02-16 11:20:23 +0530

    Added User Detail

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+let userArr = [
+    {id: 1, name:'luv'},
+    {id: 2, name:'kush'}
+];
+
 router.get('/',(req, res)=>{
     res.send('Hello world');
 });
 
 router.get('/:id',(req, res)=>{
-    res.send(`User id : ${req.params.id}`)
+    let name;
+    userArr.forEach(function(element){
+        //console.log(element.id);
+        //console.log(typeof req.params.id)
+       // console.log(typeof element.id)
+        if(req.params.id == element.id){
+            console.log(element.name);
+            name = element.name;
+        }
+    })
+    res.send(`User Detail : ${name}`);
 });
 
 
