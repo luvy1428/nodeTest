b572a1c3d4be9315c093175b326e3b55ad8b805a
luv <luvy2814@gmail.com>
2020-02-16 11:38:17 +0530

    Added country in user

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 let userArr = [
-    {id: 1, name:'luv'},
-    {id: 2, name:'kush'}
+    {id: 1, name:'luv',country:'holland'},
+    {id: 2, name:'kush',country:'london'}
 ];
 
 router.get('/',(req, res)=>{
@@ -12,6 +12,7 @@ router.get('/',(req, res)=>{
 
 router.get('/:id',(req, res)=>{
     let name;
+    let country;
     userArr.forEach(function(element){
         //console.log(element.id);
         //console.log(typeof req.params.id)
@@ -19,9 +20,10 @@ router.get('/:id',(req, res)=>{
         if(req.params.id == element.id){
             console.log(element.name);
             name = element.name;
+            country = element.country;
         }
     })
-    res.send(`User Detail : ${name}`);
+    res.send(`User name : ${name} and country: ${country}`);
 });
 
 
